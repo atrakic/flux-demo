30d9bd6f857ddb749656ee69b6e9e5b5768aa977
Admir Trakic <atrakic@users.noreply.github.com>
2022-12-10 16:22:49 +0100

    Update

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
-curl -f -i -skX GET "localhost" -o /dev/null -H"Host: httpbin.local" && echo "ok: httpbin.local"
+curl -f -i -sk -H"Host: httpbin.local" -H "accept: application/json" -X GET "http://localhost/get"
+curl -f -i -sk -H"Host: httpbin.local" -H "accept: application/json" -X GET "http://localhost/headers"
+
+curl -f -i -sk -H"Host: httpbin.local" -H "accept: application/json" -X PATCH "http://localhost/patch"
+curl -f -i -sk -H"Host: httpbin.local" -H "accept: application/json" -X POST "http://localhost/post"
+curl -f -i -sk -H"Host: httpbin.local" -H "accept: application/json" -X PUT "http://localhost/put"
